8d741e70d54806a64a7c6f0291402b050de94395
nicksolie <nick.solie1@gmail.com>
2020-05-11 14:39:03 -0400

    Added Collections table
    - Scaffolded Collections
    - Added null: false to create_collections schema
    - Added user_id to Collections

---
+++ db/migrate/20200511175102_create_collections.rb
@@ -0,0 +1,10 @@
+class CreateCollections < ActiveRecord::Migration[5.2]
+  def change
+    create_table :collections do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200511180231_add_user_to_collections.rb
@@ -0,0 +1,5 @@
+class AddUserToCollections < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :collections, :user, foreign_key: true
+  end
+end
